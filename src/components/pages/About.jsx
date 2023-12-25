de244d6ed6e437e27010ee6331d1ca77ba938d99
Shajahan <36882327+arshajahan@users.noreply.github.com>
2023-12-25 10:42:15 +0400

    Updated dceo name

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -74,7 +74,7 @@ function About() {
                     </div>
                     <br/>
                     <div className='mt-3'>
-                        <span className=' font-extrabold'>Deputy Chief Executive Officer (DCEO): Mubarak Fahad Al-Hajiri</span>
+                        <span className=' font-extrabold'>Deputy Chief Executive Officer (DCEO): Mubarak Fahhad Alhajri</span>
                         <p className=' text-justify mt-2'>Mubarak Fahad Al-Hajiri, armed with a distinguished Bachelor's degree in Mechanical Engineering from the esteemed Manchester University in the UK, showcases an unparalleled acumen in orchestrating and finalizing deals. His adeptness in the realm of business development, coupled with a wealth of international exposure, has been instrumental in propelling ENASCO to new heights. Currently donning the prestigious mantle of Deputy Chief Executive Officer (DCEO), Mr. Al Hajiri continues to weave a narrative of success and growth for the organization.</p>
                     </div>
                 </div>
