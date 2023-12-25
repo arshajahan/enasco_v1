b5b7a9516a9636222398e273de891684d1766343
Shajahan <36882327+arshajahan@users.noreply.github.com>
2023-12-25 11:52:56 +0400

    updated CEOs names

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -69,13 +69,13 @@ function About() {
                     <h2 className='text-gray-700 text-xl md:text-2xl font-extrabold mb-4'>Our Board</h2>
                     <img src={directors}  className=''/>
                     <div className='mt-3'>
-                        <span className=' font-semibold'>CEO: Muhammad Fahad Al-Hajri (right)</span>
-                        <p className=' text-justify mt-2'>ENASCO's dynamic CEO, Muhammad Fahad Al-Hajri, a Civil Engineering graduate from The University of Salford, UK, and a former standout at Kuwait Oil Company, has steered the company to prominence. Rooted in family values, dedicated to quality, and driven by excellence, Fahad has propelled ENASCO to surpass expectations, ensuring ongoing expansion and unprecedented growth.</p>
+                        <span className=' font-semibold'>CEO: Muhammad Fahhad Al-Hajri (right)</span>
+                        <p className=' text-justify mt-2'>ENASCO's dynamic CEO, Muhammad Fahhad Al-Hajri, a Civil Engineering graduate from The University of Salford, UK, and a former standout at Kuwait Oil Company, has steered the company to prominence. Rooted in family values, dedicated to quality, and driven by excellence, Fahad has propelled ENASCO to surpass expectations, ensuring ongoing expansion and unprecedented growth.</p>
                     </div>
                     <br/>
                     <div className='mt-3'>
                         <span className=' font-semibold'>Deputy CEO (DCEO): Mubarak Fahhad Mohammad Alhajri</span>
-                        <p className=' text-justify mt-2'>Mubarak Fahad Al-Hajiri, armed with a distinguished Bachelor's degree in Mechanical Engineering from the esteemed Manchester University in the UK, showcases an unparalleled acumen in orchestrating and finalizing deals. His adeptness in the realm of business development, coupled with a wealth of international exposure, has been instrumental in propelling ENASCO to new heights. Currently donning the prestigious mantle of Deputy Chief Executive Officer (DCEO), Mr. Al Hajiri continues to weave a narrative of success and growth for the organization.</p>
+                        <p className=' text-justify mt-2'>Mubarak Fahhad Mohammad Alhajri, armed with a distinguished Bachelor's degree in Mechanical Engineering from the esteemed Manchester University in the UK, showcases an unparalleled acumen in orchestrating and finalizing deals. His adeptness in the realm of business development, coupled with a wealth of international exposure, has been instrumental in propelling ENASCO to new heights. Currently donning the prestigious mantle of Deputy Chief Executive Officer (DCEO), Mr. Al Hajiri continues to weave a narrative of success and growth for the organization.</p>
                     </div>
                 </div>
 
