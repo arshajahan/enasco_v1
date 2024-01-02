270d40e49260709ed5575a82ba943559e34240f3
Shajahan <shajahan.ar09@gmail.com>
2024-01-02 10:48:57 +0400

    NDT fix

--- src/components/InDetail.jsx
+++ src/components/InDetail.jsx
@@ -39,8 +39,8 @@ function InDetail() {
                 <div className=' basis-2/6 '>
                     <div className=' '>
                     <h1 className=' text-[#2d3540] text-3xl lg:text-5xl font-extrabold mb-8 lg:w-32'>CONNECT YOUR BUSINESS WORLD.</h1>
-                    Welcome to Enasco, your trusted partner in Marine and Inspection Services. We are a pioneering force in the industry, committed to excellence, safety, and environmental responsibility. Our unwavering dedication to quality and innovation sets us apart as a reliable choice for your NDT and marine service needs.
-                        <br/><br/><br/>At Enasco, our mission is to provide cutting-edge NDT solutions and comprehensive marine services to ensure the safety, reliability, and efficiency of our clients' assets. We are driven by a passion for safeguarding the environment and enabling sustainable, long-term success for businesses in the marine sector.
+                    Welcome to Enasco, your trusted partner in Marine and Inspection Services. We are a pioneering force in the industry, committed to excellence, safety, and environmental responsibility. Our unwavering dedication to quality and innovation sets us apart as a reliable choice for your marine and inspection service needs.
+                        <br/><br/><br/>At Enasco, our mission is to provide cutting-edge marine and inspection services to ensure the safety, reliability, and efficiency of our clients' assets. We are driven by a passion for safeguarding the environment and enabling sustainable, long-term success for businesses in the marine sector.
                     </div>
                 </div>
                 <div className=' basis-3/6 self-end '>
