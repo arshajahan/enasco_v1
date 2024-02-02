784f5521acf02406bef8930ef70057003f4b00e9
Shajahan <shajahan.ar09@gmail.com>
2024-02-02 23:16:12 +0400

    check

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -19,9 +19,11 @@ function Contact() {
 
     const sendEmail = (e) => {
         e.preventDefault();
+
         const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
         const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
-
+        console.log(SERVICE_ID)
+        console.log(import.meta.env.VITE_TEMPLATE_ID)
         emailjs
             .sendForm(SERVICE_ID, TEMPLATE_ID, form.current)
             .then(
